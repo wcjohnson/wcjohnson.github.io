7d86e294866705d21c48a9b8641f6b958dc4227d
wcjohnson <wcjohnson@oigroup.net>
2020-07-13 01:34:06 -0700

    Remove known bad releases

--- lightscript-compiler/modules/registry.js
+++ lightscript-compiler/modules/registry.js
@@ -134,22 +134,6 @@ function getToolImplementation(toolApi) {
         }
       },
       {
-        name: '4.0.0-alpha.26',
-        babel: babel7_10_4,
-        lscPlugin: {
-          type: 'remote',
-          url: 'https://unpkg.com/@lightscript/webpacked@4.0.0-alpha.26/pluginToolCjs.js'
-        }
-      },
-      {
-        name: '4.0.0-alpha.27',
-        babel: babel7_10_4,
-        lscPlugin: {
-          type: 'remote',
-          url: 'https://unpkg.com/@lightscript/webpacked@4.0.0-alpha.27/pluginToolCjs.js'
-        }
-      },
-      {
         name: '4.0.0-alpha.28',
         babel: babel7_10_4,
         lscPlugin: {
